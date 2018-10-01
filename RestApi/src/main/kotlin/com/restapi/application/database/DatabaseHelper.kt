89c14fc5e5531a6ce9ae7b62679dae7b2113a944
mgraf <fhs39198@fh-salzburg.ac.at>
2018-10-01 22:26:46 +0200

    jetbrains exposed DB added

---
+++ RestApi/src/main/kotlin/com/restapi/application/database/DatabaseHelper.kt
@@ -0,0 +1,10 @@
+package com.restapi.application.database
+
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.SchemaUtils.create
+import org.jetbrains.exposed.sql.SchemaUtils.drop
+
+class DatabaseHelper() {
+
+}
