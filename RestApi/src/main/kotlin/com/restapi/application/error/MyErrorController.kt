b38943fc03d76a651eb6400b79e4e8796ea88415
mgraf <fhs39198@fh-salzburg.ac.at>
2019-01-09 21:35:32 +0100

    - Code cleaning

--- RestApi/src/main/kotlin/com/restapi/application/error/MyErrorController.kt
+++ RestApi/src/main/kotlin/com/restapi/application/error/MyErrorController.kt
@@ -17,26 +17,23 @@ class MyErrorController: ErrorController {
 
     fun MyErrorController() {}
 
-    @GetMapping(value = "/error")
+    @GetMapping(value = ["/error"])
     fun handleError(request: HttpServletRequest): String {
 
         val status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE)
 
         if (status != null) {
-
             val statusCode = Integer.valueOf(status.toString())
 
-            if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "error-404"
-            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "error-500"
+            return when (statusCode) {
+                HttpStatus.NOT_FOUND.value() -> "error-404"
+                HttpStatus.INTERNAL_SERVER_ERROR.value() -> "error-500"
+                else -> "error"
             }
         }
+
         return "error"
     }
 
-    override fun getErrorPath(): String {
-        return "/error"
-    }
-
+    override fun getErrorPath(): String = "/error"
 }
