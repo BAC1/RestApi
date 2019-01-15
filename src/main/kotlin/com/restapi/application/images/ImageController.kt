4d98319271f93c41811a05e2c06c9a0a6a256983
Graf Markus <m.graf@commend.com>
2019-01-15 15:37:01 +0100

    Code Clean

--- src/main/kotlin/com/restapi/application/images/ImageController.kt
+++ src/main/kotlin/com/restapi/application/images/ImageController.kt
@@ -9,9 +9,6 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.servlet.ModelAndView
 
-
-
-
 /**
  * This controller handles incoming http requests for displaying images
  *
